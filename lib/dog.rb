10aadc3d855cce2377acceb4248fabec497d3d8b
Christian Perez <perezchristian1012@gmail.com>
2019-11-01 01:37:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,10 +3,13 @@ class Dog
   attr_accessor :name, :breed
   attr_reader :id
 
+@@all = {}
+
   def initialize(id=nil, name:, breed:)
     @id = id
     @name = name
     @breed = breed
+    @@all << self
   end
 
   def self.create_table
