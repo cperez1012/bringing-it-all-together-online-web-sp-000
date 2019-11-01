361de4cbb1306213c61c905d7bec1d5e243aa4bb
Christian Perez <perezchristian1012@gmail.com>
2019-11-01 02:41:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -32,7 +32,7 @@ class Dog
     SQL
 
     DB[:conn].execute(sql, self.name, self.breed)
-    self = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
   end
 
   def self.create(name:, breed:)
